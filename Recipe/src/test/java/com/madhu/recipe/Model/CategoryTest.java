a197ebe71e7524f486dcda907ced50f365b428e0
rmadhu84 <r.madhu.84@gmail.com>
2018-09-14 07:55:48 -0400

    Added Tests to Recipe Application

---
+++ Recipe/src/test/java/com/madhu/recipe/Model/CategoryTest.java
@@ -0,0 +1,33 @@
+package com.madhu.recipe.Model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CategoryTest {
+	
+	Category category;
+	
+	@Before
+	public void setUp() {
+		category = new Category();
+		
+	}
+
+	@Test
+	public void testGetId() {
+		Long idValue = 4L;
+		category.setId(idValue);
+		assertEquals(idValue, category.getId());
+	}
+
+	@Test
+	public void testGetCategoryName() {
+	}
+
+	@Test
+	public void testGetRecipes() {
+	}
+
+}
---
+++ Recipe/src/test/java/com/madhu/recipe/Service/RecipeServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.madhu.recipe.Service;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.madhu.recipe.Model.Recipe;
+import com.madhu.recipe.Repositories.RecipeRepository;
+
+public class RecipeServiceImplTest {
+
+	RecipeServiceImpl recipeSerivce;
+	
+	@Mock
+	RecipeRepository RecipeRepo;
+	
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		recipeSerivce = new RecipeServiceImpl(RecipeRepo);
+	}
+
+	@Test
+	public void testGetRecipes() {
+		Recipe recipe = new Recipe();
+		Set<Recipe> recipeData = new HashSet<Recipe>();
+		recipeData.add(recipe);
+		
+		when(recipeSerivce.getRecipes()).thenReturn(recipeData);
+		Set<Recipe> recipes = recipeSerivce.getRecipes();
+		
+		assertEquals(recipes.size(), 1);
+	}
+
+}
