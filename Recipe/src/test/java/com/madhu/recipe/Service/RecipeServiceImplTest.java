4de2efacbfcb43a773dadc6ca0facc66cdd986a2
rmadhu84 <r.madhu.84@gmail.com>
2018-09-16 06:07:58 -0400

    Added a unit test for RecipeServiceImpl.

--- Recipe/src/test/java/com/madhu/recipe/Service/RecipeServiceImplTest.java
+++ Recipe/src/test/java/com/madhu/recipe/Service/RecipeServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.madhu.recipe.Service;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.HashSet;
@@ -17,10 +19,10 @@ import com.madhu.recipe.Repositories.RecipeRepository;
 public class RecipeServiceImplTest {
 
 	RecipeServiceImpl recipeSerivce;
-	
+
 	@Mock
 	RecipeRepository RecipeRepo;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
@@ -32,11 +34,12 @@ public class RecipeServiceImplTest {
 		Recipe recipe = new Recipe();
 		Set<Recipe> recipeData = new HashSet<Recipe>();
 		recipeData.add(recipe);
-		
+
 		when(recipeSerivce.getRecipes()).thenReturn(recipeData);
 		Set<Recipe> recipes = recipeSerivce.getRecipes();
-		
+
 		assertEquals(recipes.size(), 1);
+		verify(RecipeRepo, times(1)).findAll();
 	}
 
 }
