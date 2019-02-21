6c9b286cd86fd4aae759ee252f81e67ba508c3b1
rmadhu84 <r.madhu.84@gmail.com>
2019-02-20 20:02:53 -0500

    Update the script

--- Build/build.sh
+++ Build/build.sh
@@ -3,7 +3,5 @@ echo hello world
 LOGFILE=BuildRecipe
 LOGEXTENSION=log
 DATE=`date +%m%d%Y_%M%S`
-echo $DATE
-touch ${LOGFILE}_${DATE}.${LOGEXTENSION}
 
 mvn -f ../Recipe install -l ./logs/${LOGFILE}_${DATE}.${LOGEXTENSION}
